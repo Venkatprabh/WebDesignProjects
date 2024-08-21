1ecf00d4209d7134cbfe151468f134be64a47549
venkatprabh <venkatprabhu95@gmail.com>
2024-08-21 20:27:17 +0530

    first commit

---
+++ First_Project.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class First_Project{
+    public static void main(String args[]){
+        System.out.print("Hello Java");
+    }
+}
---
+++ Test.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class main {
+    public static void main(string args[]){
+        System.out.print("Hello Java");
+    }
+}
